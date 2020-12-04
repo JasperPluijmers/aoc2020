f3e1578ba51e085b7630c5b489bc9e642c2219a6
Jasper Pluijmers <jasper.pluijmers@nedap.com>
2020-12-04 08:47:06 +0100

    Bit better

--- python/day4/day.py
+++ python/day4/day.py
@@ -1,34 +1,18 @@
-import string
+import re
 
 fields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
 fields_bare = {'byr': lambda value: four_digit_valid(value, 1920, 2002),
                'iyr': lambda value: four_digit_valid(value, 2010, 2020),
                'eyr': lambda value: four_digit_valid(value, 2020, 2030),
                'hgt': lambda value: hgt_valid(value),
-               'hcl': lambda value: hcl_valid(value),
-               'ecl': lambda value: ecl_valid(value),
-               'pid': lambda value: pid_valid(value),
+               'hcl': lambda value: re.match(r'^#[\da-z]{6}$', value),
+               'ecl': lambda value: value in valid_hcl,
+               'pid': lambda value: re.match(r'^\d{9}$', value),
                'cid': lambda value: True}
 
 
-def pid_valid(value: str) -> bool:
-    try:
-        number = int(value)
-        return len(value) == 9
-    except:
-        return False
-
 valid_hcl = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-def ecl_valid(value: str) -> bool:
-    return value in valid_hcl
 
-def hcl_valid(value: str) -> bool:
-    if (value[0] == '#') and (len(value) == 7):
-        for char in value[1:]:
-            if char not in string.digits + string.ascii_lowercase:
-                return False
-            return True
-    return False
 
 def hgt_valid(value: str) -> bool:
     unit = value[-2:]
@@ -42,6 +26,7 @@ def hgt_valid(value: str) -> bool:
         return (number >= 59) & (number <= 76)
     return False
 
+
 def four_digit_valid(value: str, lower: int, upper: int) -> bool:
     try:
         digits = int(value)
@@ -52,6 +37,7 @@ def four_digit_valid(value: str, lower: int, upper: int) -> bool:
 
 with open('input.txt') as f:
     inputs = [line.replace("\n", " ") for line in f.read().split("\n\n")]
+
 
 def is_line_valid(input_line: str):
     for field in fields:
