2a9364f45b584a46f113eac3f18d1554e792aff4
Jasper Pluijmers <jasper.pluijmers@nedap.com>
2020-12-04 09:11:26 +0100

    Clean up a bit and add first section again

--- python/day4/day.py
+++ python/day4/day.py
@@ -1,15 +1,12 @@
 import re
 
-fields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
-fields_validity = {'byr': lambda value: four_digit_valid(value, 1920, 2002),
-               'iyr': lambda value: four_digit_valid(value, 2010, 2020),
-               'eyr': lambda value: four_digit_valid(value, 2020, 2030),
-               'hgt': lambda value: hgt_valid(value),
-               'hcl': lambda value: re.match(r'^#[\da-z]{6}$', value),
-               'ecl': lambda value: value in valid_hcl,
-               'pid': lambda value: re.match(r'^\d{9}$', value),
-               'cid': lambda value: True}
-
+fields_validities = {'byr': lambda value: four_digit_valid(value, 1920, 2002),
+                   'iyr': lambda value: four_digit_valid(value, 2010, 2020),
+                   'eyr': lambda value: four_digit_valid(value, 2020, 2030),
+                   'hgt': lambda value: hgt_valid(value),
+                   'hcl': lambda value: re.match(r'^#[\da-z]{6}$', value),
+                   'ecl': lambda value: value in valid_hcl,
+                   'pid': lambda value: re.match(r'^\d{9}$', value)}
 
 valid_hcl = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
@@ -39,20 +36,28 @@ with open('input.txt') as f:
     inputs = [line.replace("\n", " ") for line in f.read().split("\n\n")]
 
 
-def is_line_valid(input_line: str):
-    for field in fields:
-        if field not in input_line:
-            return False
-    entry_list = input_line.strip().split(" ")
-    for entry in entry_list:
-        field, value = entry.split(":")
-        if not fields_validity[field](value):
+def is_line_valid_weak(input_line: str) -> bool:
+    entries = {entry.split(':')[0]: entry.split(':')[1] for entry in input_line.strip().split(" ")}
+    for field in fields_validities.keys():
+        if field not in entries:
             return False
     return True
 
-valids = 0
 
-for input in inputs:
-    if is_line_valid(input):
-        valids +=1
+def is_line_valid(input_line: str) -> bool:
+    entries = {entry.split(':')[0]: entry.split(':')[1] for entry in input_line.strip().split(" ")}
+    for field, validity in fields_validities.items():
+        if not ((field in entries) and (validity(entries[field]))):
+            return False
+    return True
+
+
+valids_weak = 0
+valids = 0
+for line in inputs:
+    if is_line_valid_weak(line):
+        valids_weak += 1
+    if is_line_valid(line):
+        valids += 1
+print(valids_weak)
 print(valids)
