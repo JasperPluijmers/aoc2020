e1a102d25935592912d6467d3fca4a6ee94a5697
Jasper Pluijmers <jasper.pluijmers@nedap.com>
2020-12-08 08:49:12 +0100

    small refactor

--- python/day8/day.py
+++ python/day8/day.py
@@ -15,14 +15,25 @@ class State(Enum):
 
 
 class Console():
-    def __init__(self, program):
+    def __init__(self):
         self.accumulator = 0
         self.pointer = 0
         self.previous_pointers = set()
-        self.program = program
+        self.program = []
         self.operation_mapping = {Operation.ACC: lambda value: self.execute_acc(value),
                                   Operation.JMP: lambda value: self.execute_jmp(value),
                                   Operation.NOP: lambda value: self.execute_nop()}
+
+    def run_program(self, program):
+        self.program = program
+        self.accumulator = 0
+        self.pointer = 0
+        self.previous_pointers = set()
+        while True:
+            state, value = self.execute_step()
+            if state != State.RUNNING:
+                break
+        return state, value
 
     def execute_step(self):
         if self.pointer in self.previous_pointers:
@@ -47,30 +58,22 @@ class Console():
         self.pointer += 1
 
 
-def run_program(program):
-    console = Console(program)
-    while True:
-        state, value = console.execute_step()
-        if state != State.RUNNING:
-            break
-    return state, value
-
-
 with open('input.txt') as f:
     inputs = [re.findall(r'([a-z]+) ([+-]\d+)', line)[0] for line in f.readlines()]
 program = [(Operation(operation), int(value)) for operation, value in inputs]
 
-print(run_program(program))
+console = Console()
+print(console.run_program(program))
 for index, (operation, value) in enumerate(program):
     if operation == Operation.JMP:
         program[index] = (Operation.NOP, value)
-        state, final_value = run_program(program)
+        state, final_value = console.run_program(program)
         if state == State.FINISHED:
             print(final_value)
         program[index] = (Operation.JMP, value)
     if operation == Operation.NOP:
         program[index] = (Operation.JMP, value)
-        state, final_value = run_program(program)
+        state, final_value = console.run_program(program)
         if state == State.FINISHED:
             print(final_value)
         program[index] = (Operation.NOP, value)
