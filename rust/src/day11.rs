12c9324357061094860bb5e8dff97c02c04085fa
Jasper Pluijmers <jasper.pluijmers@nedap.com>
2020-12-11 12:00:52 +0100

    Better neighbours function

--- rust/src/day11.rs
+++ rust/src/day11.rs
@@ -1,13 +1,14 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::{thread, time};
+use std::{thread};
+use std::cmp::{min, max};
 
-const directions: [(isize, isize); 8] = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1) ];
+const DIRECTIONS: [(isize, isize); 8] = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)];
+
 pub fn day11() {
     let input = file_to_strings("input/day11/input.txt");
     let mut map: Vec<Vec<Tile>> = input.iter().map(|line| line.chars().map(string_to_tile).collect()).collect();
     let mut result = Some(map);
-    let begin = time::Instant::now();
     while result != None {
         map = result.unwrap();
         result = evolve_map(&map);
@@ -15,18 +16,26 @@ pub fn day11() {
             println!("{:?}", count_occupied(&map));
         }
     }
-    println!("Elapsed: {:?}", begin.elapsed())
+    let mut map: Vec<Vec<Tile>> = input.iter().map(|line| line.chars().map(string_to_tile).collect()).collect();
+    let mut result = Some(map);
+    while result != None {
+        map = result.unwrap();
+        result = evolve_map_2(&map);
+        if result == None {
+            println!("{:?}", count_occupied(&map));
+        }
+    }
 }
 fn count_occupied(map: &Vec<Vec<Tile>>) -> usize {
     return map.iter().flatten().filter(|tile| tile == &&Tile::OCCUPIED).count()
 }
 fn print_map(map: &Vec<Vec<Tile>>) {
-    print!("{}[2J", 27 as char);
+    std::process::Command::new("clear").status().unwrap().success();
     for row in map {
         println!(" ");
         for tile in row {
             match &tile {
-                Tile::FLOOR => print!("."),
+                Tile::FLOOR => print!(" "),
                 Tile::EMPTY => print!("L"),
                 Tile::OCCUPIED => print!("#")
             }
@@ -34,6 +43,45 @@ fn print_map(map: &Vec<Vec<Tile>>) {
     }
 }
 fn evolve_map(map: &Vec<Vec<Tile>>) -> Option<Vec<Vec<Tile>>> {
+    let mut changed = false;
+    let mut new_map: Vec<Vec<Tile>> =  Vec::new();
+    for y in 0..map.len() {
+        new_map.push(Vec::new());
+        for x in 0..map[0].len() {
+            match map[y][x] {
+                Tile::FLOOR => {
+                    new_map[y].push(Tile::FLOOR)
+                }
+                Tile::EMPTY => {
+                    if get_neighbours(map, x, y).iter().filter(|tile| tile == &&&Tile::OCCUPIED).count() == 0 {
+                        new_map[y].push(Tile::OCCUPIED);
+                        if !changed {
+                            changed = true
+                        }
+                    } else {
+                        new_map[y].push(Tile::EMPTY)
+                    }
+                }
+                Tile::OCCUPIED => {
+                    if get_neighbours(map, x, y).iter().filter(|tile| tile == &&&Tile::OCCUPIED).count() >= 4 {
+                        new_map[y].push(Tile::EMPTY);
+                        if !changed {
+                            changed = true
+                        }
+                    } else {
+                        new_map[y].push(Tile::OCCUPIED)
+                    }
+                }
+            }
+        }
+    }
+    if changed {
+        return Some(new_map)
+    }
+    return None
+}
+
+fn evolve_map_2(map: &Vec<Vec<Tile>>) -> Option<Vec<Vec<Tile>>> {
     let mut changed = false;
     let mut new_map: Vec<Vec<Tile>> =  Vec::new();
     for y in 0..map.len() {
@@ -61,7 +109,8 @@ fn evolve_map(map: &Vec<Vec<Tile>>) -> Option<Vec<Vec<Tile>>> {
                         }
                     } else {
                         new_map[y].push(Tile::OCCUPIED)
-                    }}
+                    }
+                }
             }
         }
     }
@@ -73,7 +122,7 @@ fn evolve_map(map: &Vec<Vec<Tile>>) -> Option<Vec<Vec<Tile>>> {
 
 fn get_seen_occupied_chairs(map: &Vec<Vec<Tile>>, x: usize, y: usize) -> usize {
     let mut count = 0;
-    for (dx, dy) in &directions {
+    for (dx, dy) in &DIRECTIONS {
         let mut new_x: isize = x as isize + dx;
         let mut new_y: isize = y as isize + dy;
         if (new_x < 0) | (new_y < 0) {
@@ -95,32 +144,16 @@ fn get_seen_occupied_chairs(map: &Vec<Vec<Tile>>, x: usize, y: usize) -> usize {
     return count
 }
 
-//y is top down, x is left to right
 fn get_neighbours(map: &Vec<Vec<Tile>>, x: usize, y: usize) -> Vec<&Tile> {
     let mut neighbours: Vec<&Tile> = Vec::new();
-    if x != 0 {
-        neighbours.push(&map[y][x - 1]);
-        if y!= 0 {
-            neighbours.push(&map[y - 1][x - 1]);
+    let length = map.len();
+    let width = map[0].len();
+    for xs in max(0, (x as isize) - 1) as usize..min(width, x + 2) {
+        for ys in max(0, (y as isize) - 1) as usize..min(length, y + 2) {
+            if !((xs == x) & (ys == y)) {
+                neighbours.push(&map[ys][xs])
+            }
         }
-        if y!= map.len() - 1 {
-            neighbours.push(&map[y + 1][x - 1]);
-        }
-    }
-    if x != (map[0].len() - 1) {
-        neighbours.push(&map[y][x + 1]);
-        if y!= 0 {
-            neighbours.push(&map[y - 1][x + 1]);
-        }
-        if y!= (map.len() - 1) {
-            neighbours.push(&map[y + 1][x + 1]);
-        }
-    }
-    if y != 0 {
-        neighbours.push(&map[y - 1][x])
-    }
-    if y != map.len() - 1 {
-        neighbours.push(&map[y + 1][x])
     }
     return neighbours
 }
